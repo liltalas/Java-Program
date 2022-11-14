8406e8bffc297cc4280ba3667587fd1ec71e2fb4
liltalas <zamirbekovasyrga@gmail.com>
2022-11-13 19:45:32 -0600

    found all duplicates from the array

---
+++ src/com/java/class23/FindAllDuplicatesFromArray.java
@@ -0,0 +1,22 @@
+package com.java.class23;
+// 2. Write a program to find all duplicates number from the array
+// (Implement this program to get unique numbers in the output)
+//Example
+//Input : 23, 44, 21, 23, 23, 35, 35, 23, 23, 44, 35
+//        Output : Duplicate numbers - 23, 44, 35
+public class FindAllDuplicatesFromArray {
+    public static void main(String[] args) {
+        int data[] = {23, 44, 21, 23, 23, 35, 35, 23, 23, 44, 35};
+
+        for(int i = 0; i < data.length - 1; i++){
+            for(int j = i + 1; j < data.length; j++){
+                if(data[i] == data[j]){
+                    System.out.println(data[i]);
+                    break;
+                }
+            }
+
+        }
+
+    }
+}
