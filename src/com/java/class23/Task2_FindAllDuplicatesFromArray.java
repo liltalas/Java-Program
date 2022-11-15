ddfa8c30de91db1f9e0a8d6fad2a2f1c03b4fdae
liltalas <zamirbekovasyrga@gmail.com>
2022-11-14 18:59:30 -0600

    output only unique duplicate numbers(only once)

--- src/com/java/class23/Task2_FindAllDuplicatesFromArray.java
+++ src/com/java/class23/Task2_FindAllDuplicatesFromArray.java
@@ -4,16 +4,19 @@ package com.java.class23;
 //Example
 //Input : 23, 44, 21, 23, 23, 35, 35, 23, 23, 44, 35
 //        Output : Duplicate numbers - 23, 44, 35
-public class FindAllDuplicatesFromArray {
+public class Task2_FindAllDuplicatesFromArray {
     public static void main(String[] args) {
         int data[] = {23, 44, 21, 23, 23, 35, 35, 23, 23, 44, 35};
 
         for(int i = 0; i < data.length - 1; i++){
+            int countDuplicates = 0;
             for(int j = i + 1; j < data.length; j++){
                 if(data[i] == data[j]){
-                    System.out.println(data[i]);
-                    break;
+                    countDuplicates++;
                 }
+            }
+            if(countDuplicates == 1){
+                System.out.println(data[i] + " " );
             }
 
         }
