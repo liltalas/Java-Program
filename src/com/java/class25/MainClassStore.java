ec10664bd81e14f4b3a100bab771af9a6095a57e
liltalas <zamirbekovasyrga@gmail.com>
2022-11-30 16:14:23 -0600

    added constructors to the class and one more method

--- src/com/java/class25/MainClassStore.java
+++ src/com/java/class25/MainClassStore.java
@@ -3,34 +3,38 @@ package com.java.class25;
 public class MainClassStore {
     public static void main(String[] args) {
 
-        // creating two objects
-        Store guest1 = new Store();
-        Store guest2 = new Store();
+        // creating three objects
+        Store guest1 = new Store("S", "Z", 40, 230);
+        Store guest2 = new Store("K", "OS", 15, 370);
+        Store administrator = new Store("Welcome to our store! ");
 
-        guest1.customerName = "Syrga";
+
+       // guest1.customerName = "S";
         guest1.coat = "Winter Puffer Coat";
         guest1.shoe = "North Face";
-        guest1.coatPrice = 230;
+       // guest1.coatPrice = 230;
         guest1.shoePrice = 80;
         guest1.currency = '$';
         guest1.discount = 40;
 
-        guest2.customerName = "Kaniet";
+        //guest2.customerName = "K";
         guest2.coat = "Insulated Thermal Coat";
         guest2.shoe = "Nike";
-        guest2.coatPrice = 370;
+       // guest2.coatPrice = 370;
         guest2.shoePrice = 110;
         guest2.currency = '$';
-        guest2.discount = 15;
+        // guest2.discount = 15;
 
         // calling methods of the class
         guest1.totalPrice(guest1.coatPrice + guest1.shoePrice);
         guest1.discountedPrice(guest1.discount);
+        guest1.buyTwoGetOneFree("socks"); // won't get executed due to the if stmt
 
         System.out.println("**********************************************");
 
         guest2.totalPrice(guest2.coatPrice + guest2.shoePrice);
         guest2.discountedPrice(guest2.discount);
+        guest2.buyTwoGetOneFree("Jacket");
     }
 
 }
--- src/com/java/class25/Store.java
+++ src/com/java/class25/Store.java
@@ -1,34 +1,61 @@
 package com.java.class25;
-
+// 1st task
 //  Create your own class with your example and have properties and methods inside the class
 //       Create a MainClass with the main method and use your own class to create an object, and perform
 //       some operations (call methods of the class)
+// 2nd task
+//  create three constructors in your class that is
+//  taking different parameters and create three objects
+//  in the MainClass using these constructors
+
 public class Store {
 
     // properties of store
     String customerName;
+    String customerLastName;
     String coat;
     String shoe;
-
     double coatPrice;
     double shoePrice;
     double totalPrice;
     double discount;
     char currency;
 
+    // 1st constructor
+    public Store(String name, String lastName, double payLess, double winterCoatPrice){
+        customerName = name;
+        customerLastName = lastName;
+        discount = payLess;
+        coatPrice = winterCoatPrice;
+
+    }
+
+    // 2nd constructor
+    public Store(String announcement){
+        coat = announcement;
+        System.out.println(announcement);
+        System.out.println();
+    }
+
     // methods/ actions
-    void totalPrice(double price){
+     void totalPrice(double price){
 
         totalPrice = totalPrice + price;
-        System.out.println(customerName + "'s total bill for the items: " + currency + totalPrice);
+        System.out.println(customerName + " " + customerLastName + "'s total bill for the items: " + currency + totalPrice);
         System.out.println("Items bought: \n" + coat + " " + currency + coatPrice + ";\n"
                  + shoe + " " + currency + shoePrice + ";");
     }
 
-    void discountedPrice(double discountPrice){
+     void discountedPrice(double discountPrice){
         discountPrice = totalPrice - ((totalPrice * discount) / 100);
         System.out.println("After Discount: " + discountPrice);
 
+    }
+
+    void buyTwoGetOneFree(String freeItem){
+        if(totalPrice >= 400){
+            System.out.println("You got a free gift: " + freeItem);
+        }
     }
 
 }
