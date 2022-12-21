c70c6c5ed2aae40fb12a871116e79ab7c548de22
liltalas <zamirbekovasyrga@gmail.com>
2022-12-20 21:44:48 -0600

    example for protected access modifier

--- src/com/java/class30/ExampleOfAccessModifierProtected.java
+++ src/com/java/class30/ExampleOfAccessModifierProtected.java
@@ -1,2 +1,15 @@
-package com.java.class30;public class ExampleOfAccessModifierProtected {
+package com.java.class30;
+
+import com.java.class31.Bank1;
+
+public class ExampleOfAccessModifierProtected extends Bank1{
+    public static void main(String[] args) {
+        Bank1 b = new Bank1();
+        //System.out.println(b.balance); // private
+        System.out.println(b.accName); // public
+       // System.out.println(b.accNumber); // default
+        System.out.println(nomineeDetails); // protected
+
+        // we can't use instance variable in the static method
+    }
 }
